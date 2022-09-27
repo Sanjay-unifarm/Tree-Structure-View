685264b0f68b030dcbaea8c6aa5dfe4644160e51
Sanjay-Gouda <sanjayofficial755@gmail.com>
2022-09-27 19:02:32 +0530

    Intial commit

--- src/components/TreeView.js
+++ src/components/TreeView.js
@@ -1,96 +1,69 @@
-
 import { Tree, TreeNode } from "react-organizational-chart";
 
 const TreeData = {
+  left: {
+    title: "Child1",
     left: {
-      title: "Child1",
+      title: "GrandChild1",
       left: {
-        title: "GrandChild1",
-        left:{
-            title: "Great Grand Child"
-        },
-        right:{
-            title: "Great Grand Child"
-        }
+        title: "Great Grand Child",
       },
       right: {
-        title: "GrandChild2",
-        left:{
-             title:'Great Grand Child'   
-        },
-        right:{
-            title:'Great Grand Child'
-        }
-      }
+        title: "Great Grand Child",
+      },
     },
-  
     right: {
-      title: "Child2",
+      title: "GrandChild2",
       left: {
-        title: "GrandChild2",
-        left:{
-            title: "Great Grand Child"
-        },
-        right:{
-            title: "Great Grand Child"
-        }
+        title: "Great Grand Child",
       },
       right: {
-        title: "GrandChild3",
-        
-        left :{ 
-            title: "Great Grand Child"
-        },
-        right :{ 
-            title: "Great Grand Child"
-        },
-    }
-    }
-  };
+        title: "Great Grand Child",
+      },
+    },
+  },
 
-  export const SubTree = ({node})=>{
-        if(node.left || node.right){
-            return(
+  right: {
+    title: "Child2",
+    left: {
+      title: "GrandChild2",
+      left: {
+        title: "Great Grand Child",
+      },
+      right: {
+        title: "Great Grand Child",
+      },
+    },
+    right: {
+      title: "GrandChild3",
 
-            <TreeNode label={node.title} lineBorderRadius={5} >
-                    {
-                        node.left && <SubTree node={node.left} />
-                    }
-                    {
-                        node.right && <SubTree node={node.right} />
-                    }
-            </TreeNode>
-            )
-        }
+      left: {
+        title: "Great Grand Child",
+      },
+      right: {
+        title: "Great Grand Child",
+      },
+    },
+  },
+};
 
-        return <TreeNode label={node.title}/>
-
-  }
-  
-
-  export const TreeDataView = ()=>{
-
-    return(
-        <Tree label={<div>Root</div>} >
-               <SubTree node={TreeData} />     
-
-
-
-              {/* <TreeNode label={<div>Child1</div>} >
-                    <TreeNode label={<div>GrandChild</div>}/>
-                    <TreeNode label={<div>GrandChild2</div>}/>
-                </TreeNode>  
-              <TreeNode label={<div>Child2</div>} >
-                    <TreeNode label={<div>GrandChild</div>}/>
-                    <TreeNode label={<div>GrandChild</div>}/>
-                </TreeNode>   */}
-              
-        </Tree>
-    )
-
-
-
+export const SubTree = ({ node }) => {
+  if (node.left || node.right) {
+    return (
+      <TreeNode label={node.title} lineBorderRadius={5}>
+        {node.left && <SubTree node={node.left} />}
+        {node.right && <SubTree node={node.right} />}
+      </TreeNode>
+    );
   }
 
+  return <TreeNode label={node.title} />;
+};
 
-
+export const TreeDataView = () => {
+  return (
+    <Tree label={<div>Root</div>}>
+      <SubTree node={TreeData} />
+    </Tree>
+  );
+};
